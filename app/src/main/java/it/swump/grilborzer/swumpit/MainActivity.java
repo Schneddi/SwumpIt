1f224a1e69d25aedef52e8edbed447339ae7fbef
Sergej <sergej1995@gmail.com>
2018-07-02 17:08:27 +0200

    Add hint bubbles and info button

--- app/src/main/java/it/swump/grilborzer/swumpit/MainActivity.java
+++ app/src/main/java/it/swump/grilborzer/swumpit/MainActivity.java
@@ -8,6 +8,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.InputStream;
 
@@ -16,10 +18,13 @@ public class MainActivity extends AppCompatActivity {
     /**
      * GUI components
      */
-    private Button btnPickFile;
     private ImageView imgViewFile;
+    private ImageView imgViewInfo;
+    private TextView txtViewPickFile;
+    private TextView txtViewSendFile;
 
-    public static final int PICK_FILE = 1;
+    private static final int PICK_FILE = 1;
+    private boolean isFilePicked = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,8 +34,10 @@ public class MainActivity extends AppCompatActivity {
         /**
          * Initialize all GUI components
          */
-
         imgViewFile = findViewById(R.id.imgViewFile);
+        imgViewInfo = findViewById(R.id.imgViewInfo);
+        txtViewPickFile = findViewById(R.id.txtViewPickFile);
+        txtViewSendFile = findViewById(R.id.txtViewSendFile);
 
         /**
          * Opens a dialog for the user to choose a file selector to pick a file to send.
@@ -42,6 +49,37 @@ public class MainActivity extends AppCompatActivity {
                 intent.setType("image/*");
                 intent.setAction(Intent.ACTION_GET_CONTENT);
                 startActivityForResult(Intent.createChooser(intent, "Select file"), PICK_FILE);
+            }
+        });
+
+        /**
+         * Hides the "how to send" bubble on click.
+         */
+        txtViewPickFile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                txtViewPickFile.setVisibility(View.INVISIBLE);
+            }
+        });
+
+        /**
+         * Hides the "how to pick" bubble on click.
+         */
+        txtViewSendFile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                txtViewSendFile.setVisibility(View.INVISIBLE);
+            }
+        });
+
+        /**
+         * Shows all hint bubble on click.
+         */
+        imgViewInfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                txtViewPickFile.setVisibility(View.VISIBLE);
+                if(isFilePicked) txtViewSendFile.setVisibility(View.VISIBLE);
             }
         });
     }
@@ -57,14 +95,20 @@ public class MainActivity extends AppCompatActivity {
                 InputStream inputStream = getContentResolver().openInputStream(data.getData());
                 imgViewFile.setImageBitmap(BitmapFactory.decodeStream(inputStream));
 
+                // Hide hint how to pick a file & show hint how to send file.
+                txtViewPickFile.setVisibility(View.INVISIBLE);
+                txtViewSendFile.setVisibility(View.VISIBLE);
+
+                isFilePicked = true;
                 inputStream.close();
             } catch (java.io.IOException e) {
                 e.printStackTrace();
+                Toast.makeText(this, "That didn't work out. Sorry! Please try again", Toast.LENGTH_LONG).show();
             }
         }
-        // Or display an error if data ends up being null
+        // Tooltip to prompt user to choose a file.
         else {
-
+            Toast.makeText(this, "Please choose a file to proceed.", Toast.LENGTH_SHORT).show();
         }
     }
 
